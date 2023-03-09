3f586b9a0b6fb72dbc1bc5d60202efa4195dc242
jackjack <85923524+junhyeukkwon@users.noreply.github.com>
2023-03-10 01:10:09 +0900

    change whole things
    change whole things

--- NEH_algorithm_2.py
+++ NEH_algorithm_2.py
@@ -1,54 +1,41 @@
-
-#파일 불러들기
+# 파일 불러들이기
 f = open('data.txt', 'r')
 lines = f.readlines()
 lines = list(map(lambda s : s.strip('\n'), lines))
 jobn = int(lines[0])
 machines = int(lines[1])
 
-# 위에 있는 3, 3, '', 이값을 지울 수있게 해야함.
+# 위에 있는 3, 3, '', 이값을 지울 수 있게 해야함.
 del lines[0]
 del lines[0]
 lines.remove('')
 
 # job 클래스 만들기
-
 class Job:
     def __init__(self, job_name, processing_time):
         self.job_name = job_name
         self.processing_time = processing_time
         self.total_processing_time = sum(self.processing_time)
-    
-    #클래스내 내장함수를 활용하기 위한 데코레이션 함수
+
+    # 클래스내 내장함수를 활용하기 위한 데코레이션 함수
     @classmethod
     def Sorting(cls, job_fam):
         sorting_jobs = sorted(job_fam, key=lambda x: x.total_processing_time, reverse=True)
         return sorting_jobs
 
-
 jobs = []
-
 for i in range(1, jobn+1):
     job = Job(i,list(map(int,lines[i-1].split())))
     jobs.append(job)
-
 sort_jobs = Job.Sorting(jobs)
 
-
 # 알고리즘 시작
-'''
-소팅한 job에서 processing time을 기준으로 2개의 makespan을 구함 (먼저 )
-만약 1,3이뽑아지면 13, 31로 부분 makespan을 구해서 가장 최소의 makespan의 부분 sequence를 만들고, 그 시퀀스는 고정 ,이거는 이제 삭제[1,3]
-그리고 나머지 잡에 대해서 추가 될때마다 고정되어있는 시퀀스의 사이사이 마다 추가가 되서 또 부분 makespan을 구함. 여기서 나온 부분 best 시퀀스는 고정되면서
-계속 남은 job을 추가
-'''
-
 def calc_makespan(jobs, order):
     n, m = len(jobs), len(jobs[0].processing_time)
-    #n행 m열을 만든다.
+    # n행 m열을 만든다.
     times = [[0] * m for _ in range(n)]
     for i in range(n):
-        job = order[i] - 1
+        job = order[i] -1
         for j in range(m):
             prev_time = times[i-1][j] if i > 0 else 0
             next_time = times[i][j-1] if j > 0 else 0
@@ -64,7 +51,8 @@ def NEH_algorithm(jobs):
         job = order[i] - 1
         best_pos, best_makespan = -1, float('inf')
         for j in range(i+1):
-            #step1. order에서 
+            order_copy = order.copy() # order 리스트의 복사본을 만든다.
+            # step1. order에서 
             for k in range(2):
                 temp_seq = partial_seq[:j] + [job] + partial_seq[j:]
                 if k == 1 and j < i:
@@ -72,16 +60,22 @@ def NEH_algorithm(jobs):
                 elif k == 1 and j == i:
                     temp_seq.append(temp_seq[-1])
                     temp_seq[-2], temp_seq[-1] = temp_seq[-1], temp_seq[-2]
-                makespan = calc_makespan(jobs, temp_seq)
+                # order_copy 리스트를 수정한다.
+                order_copy.remove(job+1)
+                order_copy.insert(j, job+1)
+                makespan = calc_makespan(jobs, [x-1 for x in order_copy])
                 if makespan < best_makespan:
                     best_makespan = makespan
                     best_pos = j
             if j < i and best_makespan < calc_makespan(jobs, partial_seq[:j] + [job] + partial_seq[j:]):
                 break
         partial_seq.insert(best_pos, job)
+        # order 리스트를 수정한다.
+        order.remove(job+1)
+        order.insert(best_pos, job+1)
     return [x+1 for x in partial_seq], best_makespan
 
 
 p, best_makespan = NEH_algorithm(jobs)
-print(p)
-print(best_makespan)
+
+print(p, best_makespan)
